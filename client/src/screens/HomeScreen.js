f08892e7aacd8a503fe2bd42f886accf0e33797e
Ali Piry <alipiry@icloud.com>
2018-12-16 01:53:33 +0330

    ⚡️ Update HomeScreen

--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -2,42 +2,86 @@ import React from 'react';
 import {
   StyleSheet,
   Text,
-  View,
   SafeAreaView,
   FlatList,
-  Button,
   TouchableOpacity,
-  Alert
+  View
 } from 'react-native';
-import { graphql } from 'react-apollo';
+import { graphql, compose } from 'react-apollo';
+import Icon from 'react-native-vector-icons/Ionicons';
+import Swipeout from 'react-native-swipeout';
 
 import { getTasks } from "../graphql/queries";
+import { removeTask } from "../graphql/mutations";
 
 class HomeScreen extends React.Component {
-  static navigationOptions = {
+  static navigationOptions = ({ navigation }) => ({
     title: 'Home',
+    headerRight:
+      (
+        <TouchableOpacity
+        onPress={() => navigation.navigate('Add')}
+        >
+          <Icon
+            name="ios-add"
+            size={30}
+            style={styles.headerIcon}
+          />
+        </TouchableOpacity>
+      )
+  });
+
+  handleRemoveTask = (item) => {
+    const { mutate } = this.props;
+    mutate({
+      variables: {
+        id: item.id,
+      },
+    });
   };
 
-  itemOptions = () =>
-    Alert.alert(
-      'Item',
-      'Options',
-      [
-        {text: 'Edit', onPress: () => this.props.navigation.navigate('Edit')},
-        {text: 'Remove', onPress: () => console.log('Remove Pressed')},
-        {text: 'Cancel', onPress: () => console.log('Cancel Pressed')},
-      ],
-      { cancelable: false }
-    );
+  renderItem = ({item}) => {
+    const swipeOutItems = [
+      {
+        text: 'Details',
+        onPress: () => this.props.navigation.navigate('Detail', {
+          title: item.title,
+          content: item.content
+        }),
+        backgroundColor: 'green'
+      },
+      {
+        text: 'Remove',
+        onPress: () => this.handleRemoveTask(item),
+        backgroundColor: 'red'
+      },
+      {
+        text: 'Edit',
+        onPress: () => this.props.navigation.navigate('Edit', { id: item.id }),
+        backgroundColor: 'blue'
+      }
+    ];
 
-  renderItem = ({item}) => (
-    <TouchableOpacity
-      style={styles.flatView}
-      onPress={this.itemOptions}
-    >
-      <Text style={styles.item}>{item.title.toUpperCase()}</Text>
-    </TouchableOpacity>
-  );
+    return (
+      <Swipeout
+        autoClose={true}
+        left={swipeOutItems}
+        style={styles.flatView}
+      >
+        <View>
+          <Text style={styles.item}>{item.title.toUpperCase()}</Text>
+        </View>
+      </Swipeout>
+    );
+  };
+
+  renderHeader = () => {
+    return (
+      <View>
+        <Text style={styles.headerText}>Tasks</Text>
+      </View>
+    );
+  };
 
   render() {
     const {
@@ -49,22 +93,19 @@ class HomeScreen extends React.Component {
     return (
       <SafeAreaView style={styles.container}>
         <FlatList
+          ListHeaderComponent={this.renderHeader}
           data={tasks}
           renderItem={this.renderItem}
           keyExtractor={item => item.id}
         />
-        <View style={styles.button}>
-          <Button
-            title="Add"
-            onPress={() => this.props.navigation.navigate('Add')}
-            color="white"
-          />
-        </View>
       </SafeAreaView>
     );
   }
 }
-export default graphql(getTasks)(HomeScreen);
+export default compose(
+  graphql(getTasks),
+  graphql(removeTask)
+)(HomeScreen);
 
 const styles = StyleSheet.create({
   container: {
@@ -76,11 +117,15 @@ const styles = StyleSheet.create({
     borderBottomWidth: 10,
   },
   flatView: {
-    padding: 20,
+    padding: 10,
+    backgroundColor: 'white'
   },
-  button: {
-    backgroundColor: 'black',
-    margin: 25,
-    borderRadius: 40,
+  headerIcon: {
+    marginRight: 20
+  },
+  headerText: {
+    fontSize: 35,
+    fontWeight: 'bold',
+    marginLeft: 10
   }
 });
